f9af61f7b3661c61e0074074462493a7b0211454
cmstudioapps <138110988+cmstudioapps@users.noreply.github.com>
2024-02-23 09:52:25 -0300

    Atualizar o ads.js

--- ads.js
+++ ads.js
@@ -1,3 +1,5 @@
+let adsVideo = Math.floor(Math.random()*3)+1
+let vds = localStorage.getItem("vds")
 let ads = Math.floor(Math.random()*4)+1
 let recusado = localStorage.getItem("rc")
 if (ads === 4) {
@@ -7,3 +9,11 @@ if (ads === 4) {
   }
   
 }
+
+if (adsVideo === 3 && vds == "ativado") {
+  
+ window.location.href = "videos.html"
+  
+  
+}
+
